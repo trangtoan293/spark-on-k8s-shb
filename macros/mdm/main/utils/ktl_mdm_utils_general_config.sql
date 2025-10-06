3614f376000cb9b1ed5004ed8fc4579b3060c2a9
TrangToan <trangtoan293@gmail.com>
2025-10-06 10:38:11 +0700

    add rm macros local

--- macros/mdm/main/utils/ktl_mdm_utils_general_config.sql
+++
@@ -1,9 +0,0 @@
-{%- macro ktl_mdm_utils_get_general_config(project_name) -%}
-    {{ return(context[project_name|lower+'_general_config_yml']()) }}
-{%- endmacro -%}
-
-
-{%- macro ktl_mdm_utils_get_cob_value(general_conf) -%}
-    {{ return(general_conf.get("run_date_config").get("cob_date")) }}
-{%- endmacro -%}
-
