168380b9e81a5e7a021405977c385b3a138c5bf2
QuangTM <quangtm@katalyst.vn>
2025-10-29 13:23:48 +0700

    feat: enhance table source handling with flexible reference support

--- models/mdm/ktlmdm/INDIVIDUAL/main/1_cleansing/KTLMDM_SHB_INDIVIDUAL_CLEANSING.sql
+++ models/mdm/ktlmdm/INDIVIDUAL/main/1_cleansing/KTLMDM_SHB_INDIVIDUAL_CLEANSING.sql
@@ -6,5 +6,11 @@
 {%- set lst_column_metadata_cdt = ktl_mdm_utils_metadata_get_cdt_column_lst(metadata_conf) -%}
 {%- set lst_column_model = lst_column_metadata + lst_column_metadata_cdt -%}
 {%- set rule_apply = ktl_mdm_utils_get_rule_field_apply_config(project_name = project, product = product, source = source, component='cleansing') -%}
-{%- set table_source = ref(var('shb_input_table')) -%}
+{%- set shb_input_identifier = var('shb_input_table') -%}
+{%- set shb_input_parts = shb_input_identifier.split('.') -%}
+{%- if shb_input_parts | length == 2 -%}
+    {%- set table_source = source(shb_input_parts[0], shb_input_parts[1]) -%}
+{%- else -%}
+    {%- set table_source = ref(shb_input_identifier) -%}
+{%- endif -%}
 {{ cleansing_handle(lst_column_model = lst_column_model, rule_apply = rule_apply, table_source = table_source, product_type = product, system = source, type_mode = 'cleansing') }}
