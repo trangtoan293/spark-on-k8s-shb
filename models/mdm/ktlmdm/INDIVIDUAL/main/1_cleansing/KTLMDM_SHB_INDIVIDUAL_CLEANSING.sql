6d0c1fb1fcc39aee658e92c955cdfdcbbea920b4
QuangTM <quangtm@katalyst.vn>
2025-10-29 13:32:37 +0700

    refactor: rename source variable to source_system for clarity

--- models/mdm/ktlmdm/INDIVIDUAL/main/1_cleansing/KTLMDM_SHB_INDIVIDUAL_CLEANSING.sql
+++ models/mdm/ktlmdm/INDIVIDUAL/main/1_cleansing/KTLMDM_SHB_INDIVIDUAL_CLEANSING.sql
@@ -1,11 +1,11 @@
 {%- set project = 'KTLMDM' -%}
 {%- set product = 'INDIVIDUAL' -%}
-{%- set source = 'SHB' -%}
-{%- set metadata_conf = ktl_mdm_utils_metadata_get_metadata_config(project_name = project, product_name = product, source = source, default = 'shb') -%}
+{%- set source_system = 'SHB' -%}
+{%- set metadata_conf = ktl_mdm_utils_metadata_get_metadata_config(project_name = project, product_name = product, source = source_system, default = 'shb') -%}
 {%- set lst_column_metadata = ktl_mdm_utils_metadata_get_metadata_column_lst(metadata_conf) -%}
 {%- set lst_column_metadata_cdt = ktl_mdm_utils_metadata_get_cdt_column_lst(metadata_conf) -%}
 {%- set lst_column_model = lst_column_metadata + lst_column_metadata_cdt -%}
-{%- set rule_apply = ktl_mdm_utils_get_rule_field_apply_config(project_name = project, product = product, source = source, component='cleansing') -%}
+{%- set rule_apply = ktl_mdm_utils_get_rule_field_apply_config(project_name = project, product = product, source = source_system, component='cleansing') -%}
 {%- set shb_input_identifier = var('shb_input_table') -%}
 {%- set shb_input_parts = shb_input_identifier.split('.') -%}
 {%- if shb_input_parts | length == 2 -%}
@@ -13,4 +13,4 @@
 {%- else -%}
     {%- set table_source = ref(shb_input_identifier) -%}
 {%- endif -%}
-{{ cleansing_handle(lst_column_model = lst_column_model, rule_apply = rule_apply, table_source = table_source, product_type = product, system = source, type_mode = 'cleansing') }}
+{{ cleansing_handle(lst_column_model = lst_column_model, rule_apply = rule_apply, table_source = table_source, product_type = product, system = source_system, type_mode = 'cleansing') }}
