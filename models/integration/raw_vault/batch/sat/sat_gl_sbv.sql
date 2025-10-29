4c09b103e60812b2e82c2a4857603d608399effb
TrangToan <trangtoan293@gmail.com>
2025-10-29 14:54:19 +0700

    add new sat and lnk

---
+++ models/integration/raw_vault/batch/sat/sat_gl_sbv.sql
@@ -0,0 +1,9 @@
+{{ config(
+    materialized='table',
+    file_format='iceberg'
+) }}
+
+{%- set model = dv_config('sat_gl_sbv') -%}
+{%- set dv_system = var("dv_system") -%}
+
+{{ ktl_autovault.sat_transform(model=model, dv_system=dv_system) }}
---
+++ models/integration/raw_vault/stream/sat/sat_der_gl_sbv.sql
@@ -0,0 +1,12 @@
+
+{{ config(
+    materialized='incremental',
+    file_format='iceberg',
+    incremental_strategy='merge'
+) }}
+
+{%- set model = dv_config('sat_gl_sbv') -%}
+{%- set dv_system = var("dv_system") -%}
+
+
+{{ ktl_autovault.sat_der_transform(model=model, dv_system=dv_system) }}
