eeed5a50553b9653c89964586b4a70684017b803
QuangTM <quangtm@katalyst.vn>
2025-10-29 11:30:48 +0700

    refactor: simplify metadata config map initialization

--- macros/ktl_mdm_configs/ktlmdm/ktlmdm_metadata_config.sql
+++ macros/ktl_mdm_configs/ktlmdm/ktlmdm_metadata_config.sql
@@ -1,7 +1,6 @@
 {%- macro ktlmdm_metadata_config_yml() -%}
-    {%- set config_map = {} -%}
-    {%- set config_map = config_map | combine({
-        'shb': shb_metadata_config_yml().get('KTL_MDM')
-    }) -%}
+    {%- set config_map = {
+        'KTL_MDM': shb_metadata_config_yml().get('KTL_MDM')
+    } -%}
     {{ return(config_map) }}
 {%- endmacro -%}
