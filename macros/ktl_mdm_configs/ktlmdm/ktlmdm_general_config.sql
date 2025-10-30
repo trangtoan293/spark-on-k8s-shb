c74e035d44b298ff1794fba673b6496c9b374e2b
QuangTM <quangtm@katalyst.vn>
2025-10-30 10:45:40 +0700

    feat: add match threshold configs for MDM

--- macros/ktl_mdm_configs/ktlmdm/ktlmdm_general_config.sql
+++ macros/ktl_mdm_configs/ktlmdm/ktlmdm_general_config.sql
@@ -2,6 +2,10 @@
     {%- set config_map = {
         'run_date_config': {
             'cob_date': None
+        },
+        'general_config': {
+            'match_column_cnt': 6,
+            'match_group_cnt': 3
         }
     } -%}
     {{ return(config_map) }}
