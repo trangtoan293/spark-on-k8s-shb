dc72471aaeb910fb147e2bc500f0dd9475b7b8b7
QuangTM <quangtm@katalyst.vn>
2025-10-30 10:51:55 +0700

    feat: enhance rule field config handling with flexible format support

--- macros/mdm/main/utils/ktl_mdm_utils_rule_field_apply_config.sql
+++ macros/mdm/main/utils/ktl_mdm_utils_rule_field_apply_config.sql
@@ -1,13 +1,21 @@
 
 {%- macro ktl_mdm_utils_get_rule_field_apply_config(project_name,product,source,component) -%}
     {%- for product_item in context[project_name|lower+'_rule_field_apply_config_yml']().get('KTL_MDM') -%}
-        {%- if product_item.get('product')|lower  == product|lower  -%}
-            {%- if component|lower  == 'MERGE'|lower -%}
-            
+        {%- if product_item.get('product')|lower == product|lower -%}
+            {%- if component|lower == 'merge' -%}
             {%- else -%}
                 {%- for source_item in product_item.get('source_system') -%}
-                    {%- if source_item.get('name')|lower  == source|lower  -%}
-                        {{ return(source_item.get(component|lower)) }}
+                    {%- if source_item.get('name')|lower == source|lower -%}
+                        {%- set conf = source_item.get(component|lower) -%}
+                        {# If conf already a mapping with matched_by_rules, return as-is #}
+                        {%- if conf is mapping and conf.get('matched_by_rules') is not none -%}
+                            {{ return(conf) }}
+                        {%- endif -%}
+                        {# If conf is a list of rules, wrap into expected structure #}
+                        {%- if conf is sequence and (conf is not string) -%}
+                            {{ return({'matched_by_rules': conf}) }}
+                        {%- endif -%}
+                        {{ return(conf) }}
                     {%- endif -%}
                 {%- endfor -%}
             {%- endif -%}
