1307e132e891f0b13cacf55195486d45224f3c27
QuangTM <quangtm@katalyst.vn>
2025-10-30 14:47:25 +0700

    feat: prevent duplicate pk_key columns in MDM auto match

--- macros/mdm/main/match/ktl_mdm_auto_match.sql
+++ macros/mdm/main/match/ktl_mdm_auto_match.sql
@@ -13,7 +13,9 @@
     {%- do auto_match_select_element.append("R.L_"~cob_col) -%}
     {%- do auto_match_select_element.append("T."~pk_key) -%}
     {%- for col in ktl_mdm_utils_metadata_get_master_with_cdt_errcnt_lst(metadata_conf, with_total_err_cnt=false) -%}
-        {%- do auto_match_select_element.append("T."~col) -%}
+        {%- if col|upper != pk_key -%}
+            {%- do auto_match_select_element.append("T."~col) -%}
+        {%- endif -%}
     {%- endfor -%}
     {%- do auto_match_select_element.append(ktl_mdm_utils_metadata_get_errcnt_column_lst(metadata_conf,alias="T.",with_total_err_cnt=false)|join("+")~ " AS TOTAL_ERR_CNT") -%}
     {%- do auto_match_select_element.append("CASE WHEN R.MANUAL_DUPLICATE_GROUP IS NULL THEN 0 ELSE 1 END HAS_MANUAL_MATCH") -%}
@@ -26,19 +28,25 @@
     {%- do auto_match_from_mini_select_element.append(pk_key) -%}
     {### Master key  #}
     {%- for col in ktl_mdm_utils_metadata_get_master_column_lst(metadata_conf) -%}
-        {%- do auto_match_from_mini_select_element.append("MAX(CASE WHEN "~col~"_RN = 1 THEN "~col~" ELSE NULL END) AS "~col~"") -%}
+        {%- if col|upper != pk_key -%}
+            {%- do auto_match_from_mini_select_element.append("MAX(CASE WHEN "~col~"_RN = 1 THEN "~col~" ELSE NULL END) AS "~col~"") -%}
+        {%- endif -%}
     {%- endfor -%}
     
     {### CDT of master key  #}
     {%- for col in ktl_mdm_utils_metadata_get_master_column_lst(metadata_conf) -%}
-        {%- if ktl_mdm_utils_metadata_does_column_have_cdt(metadata_conf,col) == true -%}
-            {%- do auto_match_from_mini_select_element.append("MAX(CASE WHEN "~col~"_RN = 1 THEN "~col~"_CDT ELSE NULL END) AS "~col~"_CDT") -%}
+        {%- if col|upper != pk_key -%}
+            {%- if ktl_mdm_utils_metadata_does_column_have_cdt(metadata_conf,col) == true -%}
+                {%- do auto_match_from_mini_select_element.append("MAX(CASE WHEN "~col~"_RN = 1 THEN "~col~"_CDT ELSE NULL END) AS "~col~"_CDT") -%}
+            {%- endif -%}
         {%- endif -%}
     {%- endfor -%}
 
     {### ERR_CNT  #}
     {%- for col in ktl_mdm_utils_metadata_get_master_column_lst(metadata_conf) -%}
-        {%- do auto_match_from_mini_select_element.append("MAX(CASE WHEN "~col~"_RN = 1 THEN "~col~"_ERR_CNT ELSE NULL END) AS "~col~"_ERR_CNT") -%}
+        {%- if col|upper != pk_key -%}
+            {%- do auto_match_from_mini_select_element.append("MAX(CASE WHEN "~col~"_RN = 1 THEN "~col~"_ERR_CNT ELSE NULL END) AS "~col~"_ERR_CNT") -%}
+        {%- endif -%}
     {%- endfor -%}
 
 
